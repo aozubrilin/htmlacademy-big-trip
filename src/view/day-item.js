0d0283f9abe501ec12aeb644d003abf84e00642b
aozubrilin <alex14_09@mail.ru>
2020-08-29 00:30:34 +0500

    Добавляет сортировку

--- src/view/day-item.js
+++ src/view/day-item.js
@@ -1,20 +1,21 @@
 import AbstractView from "./abstract.js";
 import {getISODateTime, getShortDate} from "../utils/event.js";
 
+const createDayInfoTemplate = (day, index) => {
+  const date = new Date(day.replace(/(\d{2})\.(\d{2})\.(\d{4})/, `$3-$2-$1`));
 
-const createDayItemTemplate = (day, index, events) => {
+  return `<span class="day__counter">${index + 1}</span>
+            <time class="day__date" datetime="${getISODateTime(date)}">${getShortDate(date)}</time>`;
+};
 
-  const eventsByDay = events.filter((event) => event.dateStart.toLocaleDateString() === day);
+const createDayItemTemplate = (day, index) => {
 
-  const date = eventsByDay[0].dateStart;
-  const datetime = getISODateTime(date);
-  const shortDate = getShortDate(date);
+  const dayInfo = day !== null ? createDayInfoTemplate(day, index) : ``;
 
   return (
     `<li class="trip-days__item  day">
       <div class="day__info">
-        <span class="day__counter">${index + 1}</span>
-        <time class="day__date" datetime="${datetime}">${shortDate}</time>
+      ${dayInfo}
       </div>
       <ul class="trip-events__list"> </ul>
      </li>`
@@ -22,14 +23,13 @@ const createDayItemTemplate = (day, index, events) => {
 };
 
 export default class DayItem extends AbstractView {
-  constructor(day, index, events) {
+  constructor(day, index) {
     super();
     this._day = day;
     this._index = index;
-    this._events = events;
   }
 
   getTemplate() {
-    return createDayItemTemplate(this._day, this._index, this._events);
+    return createDayItemTemplate(this._day, this._index);
   }
 }
--- src/view/sort.js
+++ src/view/sort.js
@@ -1,67 +1,75 @@
 import AbstractView from "./abstract.js";
 
-const ICON = `<svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
-              <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
-              </svg>`;
+const createSortTemplate = () => `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+<span class="trip-sort__item  trip-sort__item--day">Day</span>
+<div class="trip-sort__item  trip-sort__item--event">
+  <input
+    id="sort-event"
+    class="trip-sort__input  visually-hidden"
+    type="radio"
+    name="trip-sort"
+    value="sort-event"
+    checked
+  >
+  <label class="trip-sort__btn" for="sort-event">Event</label>
+</div>
+<div class="trip-sort__item  trip-sort__item--time">
+  <input
+    id="sort-time"
+    class="trip-sort__input  visually-hidden"
+    type="radio"
+    name="trip-sort"
+    value="sort-time"
+  >
+  <label class="trip-sort__btn" for="sort-time">
+    Time
+    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+      <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+    </svg>
+  </label>
+</div>
+<div class="trip-sort__item  trip-sort__item--price">
+  <input
+    id="sort-price"
+    class="trip-sort__input  visually-hidden"
+    type="radio"
+    name="trip-sort"
+    value="sort-price"
+  >
+  <label class="trip-sort__btn" for="sort-price">
+    Price
+    <svg class="trip-sort__direction-icon" width="8" height="10" viewBox="0 0 8 10">
+      <path d="M2.888 4.852V9.694H5.588V4.852L7.91 5.068L4.238 0.00999987L0.548 5.068L2.888 4.852Z"/>
+    </svg>
+  </label>
+</div>
+<span class="trip-sort__item  trip-sort__item--offers">Offers</span>
+</form>`;
 
-const createSortItemsTemplate = (sortItems) => {
-  return sortItems.map((it) =>
-    !it.isInput ? `<span class="trip-sort__item  trip-sort__item--${it.name.toLowerCase()}">${it.name}</span>` : `<div class="trip-sort__item  trip-sort__item--time">
-        <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${it.name.toLowerCase()}" ${it.isChecked ? `checked` : ``}>
-        <label class="trip-sort__btn" for="sort-${it.name.toLowerCase()}">
-        ${it.name}
-        ${!it.hasIcon ? ICON : ``}
-        </label >
-        </div>`).join(``);
-};
-
-const createSortTemplate = () => {
-
-  const sortItems = [
-    {
-      name: `Day`,
-      hasIcon: false,
-      isChecked: false,
-      isInput: false,
-    },
-    {
-      name: `Event`,
-      hasIcon: false,
-      isChecked: true,
-      isInput: true,
-    },
-    {
-      name: `Time`,
-      hasIcon: true,
-      isChecked: false,
-      isInput: true,
-    },
-    {
-      name: `Price`,
-      hasIcon: true,
-      isChecked: false,
-      isInput: true,
-    },
-    {
-      name: `Offers`,
-      hasIcon: false,
-      isChecked: false,
-      isInput: false,
-    }
-  ];
-
-  const sortItemsTemplate = createSortItemsTemplate(sortItems);
-
-  return (
-    `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-    ${sortItemsTemplate}
-    </form>`
-  );
-};
 
 export default class Sort extends AbstractView {
+  constructor() {
+    super();
+
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (evt.target.tagName !== `INPUT`) {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.value);
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener(`change`, this._sortTypeChangeHandler);
   }
 }
 
